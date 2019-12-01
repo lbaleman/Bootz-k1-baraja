20b2a3565a3428af91311580e0b123939854a694
Luis Bastos <luisba93@gmail.com>
2019-12-01 09:51:14 +0000

    Primera prueba funcional

--- baraja.py
+++ baraja.py
@@ -1,3 +1,4 @@
+import random
 _palos = ['o','c','e','b']
 _cartas = ['A','2','3','4','5','6','7','S','C','R']
 
@@ -6,5 +7,17 @@ def baraja():
     for palo in _palos:
         for carta in _cartas:
             result.append(carta + palo)
-    
+
     return result
+
+def elige_carta(i,longitud):
+    return random.randint(0, longitud-1)
+
+def mezclar(b):
+    for i in range(len(b)):
+        azar = elige_carta(i, len(b))
+        aux  = b[i]
+        b[i] = b[azar]
+        b[azar] = aux
+
+    return b
--- baraja_test.py
+++ baraja_test.py
@@ -1,5 +1,12 @@
 import unittest
+from unittest.mock import patch
 import baraja
+
+def mi_eligecarta(i,longitud):
+    if i == longitud-1:
+        return 0
+    else:
+        return i+1
 
 class BarajaTest(unittest.TestCase):
 
@@ -8,6 +15,13 @@ class BarajaTest(unittest.TestCase):
         self.assertEqual(len(b),40)
         self.assertEqual(b[0],'Ao')
         self.assertEqual(b[39],'Rb')
+
+    @patch('baraja.elige_carta', mi_eligecarta)
+    def test_mezclar_baraja(self):
+        b = ['A','B','C','D','E']
+        a = baraja.mezclar(b)
+        mezclado = ['A','C','D','E','B']
+        self.assertEqual(a,mezclado)
 
 if __name__ == "__main__":
     unittest.main()
