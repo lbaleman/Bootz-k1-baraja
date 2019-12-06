f62b146e661f248be9d8ec05c7c0edaa013d5763
Luis Bastos <luisba93@gmail.com>
2019-12-06 13:20:19 +0000

    Ejercicio baraja finalizado

--- baraja.py
+++ baraja.py
@@ -7,7 +7,6 @@ def baraja():
     for palo in _palos:
         for carta in _cartas:
             result.append(carta + palo)
-
     return result
 
 def elige_carta(i,longitud):
@@ -21,3 +20,53 @@ def mezclar(b):
         b[azar] = aux
 
     return b
+
+class Baraja():
+    naipes = []
+    __palos = ['o','c','e','b']
+    __cartas = ['A','2','3','4','5','6','7','S','C','R']
+
+    def __init__(self):
+        self.naipes = []
+        for palo in self.__palos:
+            for carta in self.__cartas:
+                self.naipes.append(carta + palo)
+
+    def elige_carta(self, i):
+        return random.randint(0, len(self.naipes)-1)
+
+    # No hace falta poner def mezclar(self, naipe), porque naipe es un atributo de la clase y ya la tenemos creada#
+    # seria redundante. Mi instancia va a estar definida por sus naipes. No hace falta return porque al tener#
+    # self delante es un atributo referido a nuestra variable
+    def mezclar(self):
+        for i in range(len(self.naipes)):
+            azar = self.elige_carta(i)
+            aux  = self.naipes[i]
+            self.naipes[i] = self.naipes[azar]
+            self.naipes[azar] = aux
+
+    def repartir(self, cartas, jugadores):
+        jugadas = []
+        for i in range(jugadores):
+            jugadas.append([])
+            for j in range(cartas):
+                jugadas[i].append(self.naipes[j*jugadores + i])
+
+        repartidas = cartas*jugadores
+        self.naipes = self.naipes[repartidas:]
+
+        return jugadas
+    
+    def repartirRamon(self, cartas, jugadores):
+        jugadas=[]
+        for i in range(jugadores):
+            jugadas.append([])
+        
+        for j in range(cartas):
+            for item in jugadas:
+                cartas=self.naipes.pop(0)
+                item.append(cartas)
+        return jugadas
+
+     
+    
--- baraja_test.py
+++ baraja_test.py
@@ -2,28 +2,78 @@ import unittest
 from unittest.mock import patch
 import baraja
 
-def mi_eligecarta(i,longitud):
+def mi_eligecarta(i, longitud):
     if i == longitud-1:
         return 0
     else:
         return i+1
 
-class BarajaTest(unittest.TestCase):
+
+class BarajaFuncionalTest(unittest.TestCase):
 
     def test_crear_baraja(self):
         b = baraja.baraja()
-        self.assertEqual(len(b),40)
-        self.assertEqual(b[0],'Ao')
-        self.assertEqual(b[39],'Rb')
+        self.assertEqual(len(b), 40)
+        self.assertEqual(b, ['Ao', '2o', '3o', '4o', '5o', '6o', '7o', 'So', 'Co', 'Ro', 'Ac', '2c', '3c', '4c', '5c', '6c', '7c', 'Sc', 'Cc', 'Rc', 'Ae', '2e', '3e', '4e', '5e', '6e', '7e', 'Se', 'Ce', 'Re', 'Ab', '2b', '3b', '4b', '5b', '6b', '7b', 'Sb', 'Cb', 'Rb'])
+
+        self.assertEqual(b[0], 'Ao')
+        self.assertEqual(b[39], 'Rb')
+        self.assertEqual(b[10], 'Ac')
+        self.assertEqual(b[20], 'Ae')
 
     @patch('baraja.elige_carta', mi_eligecarta)
-    def test_mezclar_baraja(self):
-        b = ['A','B','C','D','E']
-        a = baraja.mezclar(b)
-        mezclado = ['A','C','D','E','B']
-        self.assertEqual(a,mezclado)
+    def test_mezclar_lista(self):
+        b = ['A', 'B', 'C', 'D', 'E']
+        mezclada = ['A', 'C', 'D', 'E', 'B']
+        b = baraja.mezclar(b)
+        self.assertEqual(b, mezclada)
 
-if __name__ == "__main__":
+def class_eligecarta(self, i):
+    return mi_eligecarta(i, len(self.naipes))
+
+class BarajaObjetoTest(unittest.TestCase):
+    def test_crear_baraja(self):
+        b = baraja.Baraja()
+        self.assertEqual(len(b.naipes), 40)
+        self.assertEqual(b.naipes, ['Ao', '2o', '3o', '4o', '5o', '6o', '7o', 'So', 'Co', 'Ro', 'Ac', '2c', '3c', '4c', '5c', '6c', '7c', 'Sc', 'Cc', 'Rc', 'Ae', '2e', '3e', '4e', '5e', '6e', '7e', 'Se', 'Ce', 'Re', 'Ab', '2b', '3b', '4b', '5b', '6b', '7b', 'Sb', 'Cb', 'Rb'])
+
+        self.assertEqual(b.naipes[0], 'Ao')
+        self.assertEqual(b.naipes[39], 'Rb')
+        self.assertEqual(b.naipes[10], 'Ac')
+        self.assertEqual(b.naipes[20], 'Ae')
+
+    @patch('baraja.Baraja.elige_carta', class_eligecarta)
+    def test_mezclar_lista(self):
+        b = baraja.Baraja()
+        b.mezclar()
+
+        self.assertEqual(b.naipes[0], 'Ao')
+        self.assertEqual(b.naipes[39], '2o')
+        self.assertEqual(b.naipes[10], '2c')
+        self.assertEqual(b.naipes[20], '2e')
+
+    def test_mezclar_sin_comprobar_orden(self):
+        b = baraja.Baraja()
+        b.mezclar()
+
+        self.assertEqual(len(b.naipes), 40)
+
+    def test_repartir_baraja(self):
+        b = baraja.Baraja()
+        jugadas = b.repartir(3,2)
+        self.assertEqual(len(jugadas), 2)
+        self.assertEqual(len(b.naipes), 34)
+        self.assertEqual(jugadas[0], ['Ao', '3o', '5o'])
+        self.assertEqual(jugadas[1], ['2o', '4o', '6o'])
+
+    def test_repartirRamon_baraja(self):
+        b = baraja.Baraja()
+        jugadas = b.repartirRamon(5,3)
+        self.assertEqual(len(jugadas),3)
+        self.assertEqual(len(b.naipes),25)
+        self.assertEqual(jugadas[0], ['Ao', '4o', '7o','Ro','3c'])
+        self.assertEqual(jugadas[1], ['2o', '5o', 'So','Ac','4c'])
+        self.assertEqual(jugadas[2], ['3o', '6o', 'Co','2c','5c'])
+
+if __name__ == '__main__':
     unittest.main()
-
-    
